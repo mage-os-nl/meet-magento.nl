7f982199b2da981173ef45a576f0cc3d0cb0012a
jisse Reitsma <jisse@yireo.com>
2023-04-29 16:26:22 +0200

    Change master branch

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yarn build
 git commit -m 'New build' public/build/
-git push origin main
-ssh yireo-php "cd /home/yireo/public_html/meet-magento.nl && git pull origin main && composer install"
+git push origin master
+ssh yireo-php "cd /home/yireo/public_html/meet-magento.nl && git pull origin master && composer install"
 yireo opcache:refresh 8.1
